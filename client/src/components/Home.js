ddbf7ed986f90db6dde30cd5333c9c62434b7d40
gabs-v <gabyvazquez8297@gmail.com>
2022-12-22 18:16:34 -0800

    Home Components

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,14 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            poop
+            <div className='navbar'>
+                <ul>
+                    <li>About Me</li>
+                    <li>Resume</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
         </div>
     )
 }
